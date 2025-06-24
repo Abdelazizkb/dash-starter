36c5dc1ea25a8b53437d3278c187a63e14c86669
Abdelazizkb <61007391+Abdelazizkb@users.noreply.github.com>
2025-06-25 00:47:28 +0100

    add header

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,1 +1,3 @@
+/* eslint-disable react-refresh/only-export-components */
 export { default as Loader } from "./loader";
+export * from "./ui/button";
--- src/layout/dashboard/header.tsx
+++ src/layout/dashboard/header.tsx
@@ -1,7 +1,18 @@
+import { Button } from "@/components";
+import iconUser from "@/assets/user.png";
+
 const Header = () => {
   return (
     <div className="flex justify-between items-center h-14 w-screen py-2 px-4 bg-white border-b-1">
-      header
+      <p>Dash-Starter</p>
+      <div className="flex gap-2">
+        <Button variant="ghost">Search</Button>
+        <Button variant="ghost">Help</Button>
+        <Button variant="ghost">Explore apps</Button>
+        <Button variant="ghost">Contact Support</Button>
+        <Button>Upgrade</Button>
+        <img className="h-9 w-9" src={iconUser} alt="iconUser.png" />
+      </div>
     </div>
   );
 };
